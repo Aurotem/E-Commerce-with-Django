708a034c9e70c1b466974a34c8a4ba0c0b76e67f
Melihcan Fidan <melihcan.fi@gmail.com>
2024-01-15 14:13:10 +0300

    Setup Process

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':[BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,13 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-TEMPLATES = [
-    {
-        'DIRS':[BASE_DIR / 'templates'],
-    }
-]
 
 COMPRESS_ROOT = BASE_DIR / 'static'
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './templates/**/*.html'
+      './templates/**/*.html',
+      './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
