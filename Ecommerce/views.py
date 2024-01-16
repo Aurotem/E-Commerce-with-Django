30bb2161493caaf200ecea64a474e3835a6ce2e4
Melihcan Fidan <melihcan.fi@gmail.com>
2024-01-16 15:45:29 +0300

    update

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+import json
+import urllib.request
+
+
 
 def index(request):
     return render(request, 'index.html')
