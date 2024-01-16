41ac6cd91d3b18eb12a77855b63533560221ae00
Melihcan Fidan <melihcan.fi@gmail.com>
2024-01-16 16:09:42 +0300

    update

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -7,6 +7,6 @@ def index(request):
     response = requests.get('https://api.escuelajs.co/api/v1/products')
     data = response.json()  # Assuming the API returns JSON data
     context = {
-        'data':data
+        'products':data
     }
     return render(request, 'index.html',context)
